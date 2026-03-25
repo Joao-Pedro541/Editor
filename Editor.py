b6695bf9d29c5175687cbd2d4848aaee3678d54f
João Pedro <103468923+Joao-Pedro541@users.noreply.github.com>
2026-03-25 17:50:55 -0300

    little ajust in editor

--- Editor.py
+++ Editor.py
@@ -1,51 +1,95 @@
 import moviepy 
 import whisper
 
-def VideoEditor():
-    takes = {"take 1": []}
-    actualTake = "take 1"
+
+class Editor():
+
+    def __init__(self):
+        self.clips = []
+
+        self.takes = {f"take {1}": []}
+        self.actualTake = f"take {1}"
+        self.takesRange = 2
+
+    def trascription(self):
+        
+        
+        videoClip = moviepy.VideoFileClip("video1.mp4")
+        videoClip.audio.write_audiofile("audio1.mp3")
+
+        audioClip = moviepy.AudioFileClip("audio1.mp3")
+
+        model = whisper.load_model("turbo")
+        result = model.transcribe("audio1.mp3", fp16=False)
+
+        isAPause = False
+
+        for i in range(len(result["segments"])):
+            words = result["segments"][i]["text"].lower().split()
+            
+            actualWords = []
+            for w in words:
+                actualWords.append(w.replace(",", ""))
+            print(f"{result['segments'][i]['start']}: {actualWords} end: {result['segments'][i]['end']}")
+
+            if "pause" in actualWords:
+                isAPause = True
+                continue
+            elif "return" in actualWords:
+                isAPause = False
+                continue
+            elif "take" in actualWords:
+                idx = actualWords.index("take")
+                take_number = actualWords[idx + 1]
+                self.actualTake = f"take {take_number}"
+
+                if self.actualTake not in self.takes:
+                    self.takes[self.actualTake] = []
+                self.takes[self.actualTake].clear()
+                continue
+
+            elif "silence" in actualWords:
+                self.takes[self.actualTake].append([result["segments"][i]["end"],result["segments"][i + 1]["start"]])
+                continue
+            if isAPause:
+                continue
+                
+            if not isAPause: 
+                self.takes[self.actualTake].append([result["segments"][i]["start"], result["segments"][i]["end"]])
     
-    videoClip = moviepy.VideoFileClip("video1.mp4")
-    videoClip.audio.write_audiofile("audio1.mp3")
 
-    audioClip = moviepy.AudioFileClip("audio1.mp3")
+    def audioEditor(self, audio: moviepy.AudioClip):
 
-    model = whisper.load_model("turbo")
-    result = model.transcribe("audio1.mp3", fp16=False)
+        self.trascription()
 
-    for segments in result["segments"]:
-        words = segments["text"].lower().split()
-        print(words)
-        isAtake = False
-        if "take" in words:
-            idx = words.index("take")
-            take_number = words[idx + 1]
-            actualTake = f"take {take_number}"
+        for tk in self.takes.values():
+            for start, end in tk:
+                start = max(0, start)
+                end = min(end, audio.duration)
 
-            if actualTake not in takes:
-                takes[actualTake] = []
-            isAtake = True
+                if start < end: 
+                    self.clips.append(audio.subclipped(start, end))
 
-        if isAtake == True:
-            takes[actualTake].clear()
-            takes[actualTake] = []
-        else: 
-            takes[actualTake].append([segments["start"], segments["end"]])
+        finalAudio = moviepy.concatenate_audioclips(self.clips)
+        finalAudio.write_audiofile("finalAudioClip.mp3")
 
-    clips = []
-    for tk in takes.values():
-        for start, end in tk:
-            start = max(0, start)
-            end = min(end, videoClip.duration)
+    def VideoEditor(self, videoClip: moviepy.AudioClip):
+        self.trascription()
 
-            if start < end: 
-                clips.append(videoClip.subclipped(start, end))
+        clips = []
+        for tk in self.takes.values():
+            for start, end in tk:
+                print(f"{start} + end: + {end}")
+                start = max(0, start)
+                end = min(end, videoClip.duration)
 
-    finalVideo = moviepy.concatenate_videoclips(clips)
-    finalVideo.write_videofile("final_clip.mp4")  
+                if start < end: 
+                    clips.append(videoClip.subclipped(start, end))
 
-def main():
-    VideoEditor()
+        finalVideo = moviepy.concatenate_videoclips(clips)
+        finalVideo.write_videofile("finalVideoClip.mp4")  
+
+editor = Editor()
 
 if __name__ == "__main__":
-    main()
+    editor.VideoEditor(moviepy.VideoFileClip("video1.mp4"))
